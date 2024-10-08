5c6a4d3dacdbede858ec0ce671f17f52fb0af3e5
nikon <kozlovnikon907@gmail.com>
2024-10-08 20:47:15 +0200

    new1

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-let toogle = document.querySelector('.lang-toogle');
-let popup = document.querySelector('.popup');
-let english = document.querySelector('.lang-button')
-
-toogle.addEventListener('click' , function (event) {
-	if (popup.classList.contains('disp1') == true) {
-		popup.classList.remove('disp1')
-	}
-	else {
-		popup.classList.add('disp1') 
-	}
-})
-
-popup.addEventListener('click' , function (event) {
-	if (popup.classList.contains('disp1') == true) {
-		english.classList.add('popup')
-		
-		popup.classList.remove('popup')
-		popup.classList.add('lang-button')
-	}
-})
-
-
+let eng = document.querySelector('#en')
+let homeBtn = document.querySelector('#home')
+homeBtn.classList.add('on')
+eng.classList.add('on')
+console.log(homeBtn)
 
 
 
