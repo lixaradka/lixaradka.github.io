577a97c162cdd8c3503ebf81ea4d1db31fa78c8e
nikon <kozlovnikon907@gmail.com>
2024-10-09 23:53:49 +0200

    09 commit

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 let eng = document.querySelector('#en')
 let homeBtn = document.querySelector('#home')
+
+let images = document.querySelectorAll('.slider-img')
+let controlls = document.querySelectorAll('.controlls')
+let imgIndex = 0;
+
+function show(index) {
+    images[imgIndex].classList.remove('active');
+    images[index].classList.add('active');
+    imgIndex = index
+}
+
+controlls.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (event.target.classList.contains('prev')) {
+            let index = imgIndex - 1;
+
+            if(index < 0) {
+                index = images.length - 1;
+            }
+            show(index);
+        } else if (event.target.classList.contains('next')) {
+            let index = imgIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    })
+})
+
+show(imgIndex)
+
+
+
 homeBtn.classList.add('on')
 eng.classList.add('on')
 console.log(homeBtn)
